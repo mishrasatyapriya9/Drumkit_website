bb62b4b6dbe1a69fceba335e0ac6e12f65e2ab1c
Satyapriya mishra <106869525+mishrasatyapriya9@users.noreply.github.com>
2023-04-04 01:14:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+// detexting button press
+var numofdrum = document.querySelectorAll(".drum").length;
+for(var i=0;i<numofdrum;i++)
+{
+    // document.querySelectorAll("button")[i].addEventListener("click",handleclick); 
+    // function handleclick(){ 
+    // alert("i got clicked")
+    // var audio = new Audio("sounds/tom-1.mp3");
+    // audio.play();
+    // this.style.color="white";
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+
+    
+    var buttonclick = this.innerHTML;
+    makesound(buttonclick)
+    buttonanimation(buttonclick);
+
+});
+}
+// detexting key press
+document.addEventListener("keypress",function(event){
+    makesound(event.key);
+    buttonanimation(event,key);
+});
+function makesound(key){
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        
+            default: console.log(buttonclick)
+            break;
+}
+}
+function buttonanimation(currentkey){
+    var activebutton = document.querySelector("."+currentkey);
+    activebutton.classList.add(pressed);
+    setTimeout(function(){
+        activebutton.classList.remove("pressed");
+    },100);
+}
+
+// document.querySelectorAll("button")[i].addEventListener("click",function(){
+//     alert("i got clicked")
+// });
